4216c6f649ad070a0d177063db4ffafeb3ceed1a
zmej-gorunuch <z.g-web@ukr.net>
2020-09-16 15:53:31 +0300

    add responsive page menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
+// Menu add no-scroll
 $('.menu__menu-toggle input').prop('checked', false).click(function () {
     if ($('.menu__menu-toggle input').is(':checked')) {
         $('body').toggleClass('no-scroll');
     } else {
         $('body').toggleClass('no-scroll');
     }
+});
+
+// Open mobile menu
+$('.mobile_menu').click(function(e){
+    e.preventDefault();
+    $('.header__menu .mobile_menu_list').slideToggle();
 });
